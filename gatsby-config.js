0cba81815f6e57bdf86fd40dff0378df23c30c1c
Anvar <mail@anvar.in>
2018-09-20 15:25:22 +0400

    After WP Compilation

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,19 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Default Starter',
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
+    siteMetadata: {
+        title: 'Wordpress Gatsby',
+        subtitle: `Fetch Data From Local WP Install`,
     },
-    'gatsby-plugin-offline',
-  ],
-}
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: "gatsby-source-wordpress",
+            options: {
+                baseUrl: "wp.arfad.in",
+                protocol: "http",
+                hostingWPCOM: false,
+                useACF: true,
+                verboseOutput: true
+            }
+        }
+    ],
+};
